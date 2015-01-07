c3b26173765261b1b4f13739cfe7e3c8b7a3b8a6
Michael Barton <mail@michaelbarton.me.uk>
2015-01-07 12:26:05 -0800

    Add CIDER plugin to lein profiles
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user
- {:plugins       [[com.aphyr/prism "0.1.1"]]
+ {:plugins       [[com.aphyr/prism   "0.1.1"]
+                  [cider/cider-nrepl "0.8.2"]]
   :dependencies  [[com.aphyr/prism "0.1.1"]
                   [slamhound       "1.5.5"]]}
   :aliases       {"slamhound"  ["run" "-m" "slam.hound"]}}}
