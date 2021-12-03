6b7399d20f19319505be798c73af77c1c570e9c1
Michael Barton <mail@michaelbarton.me.uk>
2021-12-03 10:28:05 -0500

    Reformat files

--- wiki/convert_daily_log.py
+++ wiki/convert_daily_log.py
@@ -12,7 +12,7 @@ import textwrap
 import click
 import bs4
 
-EDITOR = os.environ.get('EDITOR','vim')
+EDITOR = os.environ.get("EDITOR", "vim")
 
 
 @click.command()
@@ -27,23 +27,22 @@ def run(filepath: str, skip_edit: bool):
     # Parse dateline
     # Should throw an error if not correctly formatted
     date_subject = datetime.strptime(msg.get("Subject"), "%Y%m%d")
-    date_str = date_subject.strftime('%Y%m%d')
+    date_str = date_subject.strftime("%Y%m%d")
 
     # Convert html contents to plain text
     email_body = str(msg.get_content()).replace("=\n", "")
     soup = bs4.BeautifulSoup(email_body, features="html.parser")
 
     # Strip out weird characters and footer.
-    body = re.sub("--Sent from.+", "", soup.get_text().replace("=E2=80=A2 =E2=80=A2 =E2=80=A2", "\n"))
+    body = re.sub(
+        "--Sent from.+", "", soup.get_text().replace("=E2=80=A2 =E2=80=A2 =E2=80=A2", "\n")
+    )
 
     # Wrap text to 80 chars
     body = f"# [[{date_str}]]\n" + body.replace("#", "\n\n#")
 
     # Write to file
-    dst_file = (
-        pathlib.Path.home()
-        / f"Dropbox/wiki/unprocessed/{date_str}_daily_log.md"
-    )
+    dst_file = pathlib.Path.home() / f"Dropbox/wiki/unprocessed/{date_str}_daily_log.md"
     dst_file.write_text(body)
 
     if skip_edit:
@@ -52,5 +51,5 @@ def run(filepath: str, skip_edit: bool):
     subprocess.call([EDITOR, str(dst_file.absolute())])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
