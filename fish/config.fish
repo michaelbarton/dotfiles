95fc5eacab37b7a1de4fd43a2a6794e34d9ee0f5
Michael Barton <mail@michaelbarton.me.uk>
2022-12-27 15:53:09 +0000

    Update code comments

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,8 @@ bass source ~/.bashrc
 
 starship init fish | source
 
+# This needs to be a function instead of alias, because mosh calls ssh. If it's an
+# alias then it calls itself
 function ssh
 	mosh $argv
 end
