88bbad7efe0ebb2d28e5f10462b763b6b754bbc7
Michael Barton <mail@michaelbarton.me.uk>
2023-04-04 11:55:32 -0700

    Add git script to clean branches

--- git/clean_branches.sh
+++ git/clean_branches.sh
@@ -4,7 +4,8 @@
 #
 
 set -o errexit
-set -o nouset
+
+#!/bin/bash
 
 # Check if Git is installed
 if ! git --version >/dev/null 2>&1; then
@@ -18,6 +19,12 @@ if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
   exit 1
 fi
 
+# Check for the -f flag
+force_delete=false
+if [[ "$1" == "-f" ]]; then
+  force_delete=true
+fi
+
 # Fetch the latest changes from the remote repository and prune deleted branches
 git fetch --prune
 
@@ -27,7 +34,12 @@ merged_branches=$(git branch --merged | grep -v "\*" | grep -v "^\s*master" | gr
 # Iterate through each local branch and check if it exists in the list of merged branches
 for branch in $(git for-each-ref --format='%(refname:short)' refs/heads); do
   if ! echo "$merged_branches" | grep -q "^$branch$"; then
-    echo "Skipping branch '$branch' as it either doesn't exist on the origin or isn't merged."
+    if [ "$force_delete" = true ]; then
+      echo "Force deleting branch '$branch' as it either doesn't exist on the origin or isn't merged."
+      git branch -D "$branch" 2>/dev/null || echo "Error: Could not delete branch '$branch'."
+    else
+      echo "Skipping branch '$branch' as it either doesn't exist on the origin or isn't merged."
+    fi
   else
     echo "Deleting branch '$branch' as it is merged and doesn't exist on the origin."
     git branch -d "$branch" 2>/dev/null || echo "Error: Could not delete branch '$branch'."
