a850320868d384270b4bb2b309fb4920a5dd9b96
Michael Barton <mail@michaelbarton.me.uk>
2023-04-05 11:40:19 -0700

    Add script to create PR on GH using CHANGELOG

---
+++ github/create_pr.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Create a pull request on GitHub using the CHANGELOG as a diff
+
+set -o errexit
+set -o nounset
+
+# Is there a branch called master or main?
+if git branch -a | grep 'remotes/origin/master'; then
+  BASE=master
+elif git branch -a | grep 'remotes/origin/main'; then
+  BASE=main
+else
+  echo "No master or main branch found"
+  exit 1
+fi
+
+FILE=$(mktemp -d)/pull_request.txt
+
+cat > $FILE <<- EOM
+\`\`\`diff
+$(git diff ${BASE} -- CHANGELOG.md)
+\`\`\`
+EOM
+
+git promote
+gh pr create --body-file="${FILE}" "$@"
