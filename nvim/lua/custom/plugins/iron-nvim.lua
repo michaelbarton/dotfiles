9b2ea67da567955a4ddac7d835cdb21f802bf74f
Michael Barton <mail@michaelbarton.me.uk>
2025-03-12 21:25:17 -0700

    Temporary fix for iron.vim

--- nvim/lua/custom/plugins/iron-nvim.lua
+++ nvim/lua/custom/plugins/iron-nvim.lua
@@ -1,21 +1,4 @@
 return {
   'Vigemus/iron.nvim',
   version = '*',
-  config = function()
-    require('iron.core').setup {
-      config = {
-        repl_definition = {
-          sh = {
-            -- Can be a table or a function that
-            -- returns a table (see below)
-            command = { 'zsh' },
-          },
-          python = {
-            command = { 'python3' },
-            block_deviders = { '# %%', '#%%' },
-          },
-        },
-      },
-    }
-  end,
 }
